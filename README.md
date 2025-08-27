346aa3960ce618ea31899e772d4c39e4d8df602e
vitorcbm92 <138312491+vitorcbm92@users.noreply.github.com>
2025-08-27 18:39:51 +0000

    develop - First message on home page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clone-tabnews
 A clone of tabnews project.
+
+# To install nvm version:
+    - nvm install
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Test</h1>
+}
+// export the function from this file
+export default Home;
